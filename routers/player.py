8cd4aadc1ee71f09826f7c404742fbe51b9d1800
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-02-02 00:46:20 -0600

    initial

--- routers/player.py
+++ routers/player.py
@@ -1,12 +1,14 @@
 import datetime
 import re
 import time
-
+import asyncio
 import pendulum as pend
 import ujson
 import coc
+import snappy
+import aiohttp
 
-from collections import defaultdict
+from collections import defaultdict, deque
 from fastapi import  Request, Response, HTTPException
 from fastapi import APIRouter
 from fastapi_cache.decorator import cache
@@ -275,14 +277,36 @@ async def search_players(name: str, request: Request, response: Response):
 
 @router.post("/player/bulk",
           tags=["Player Endpoints"],
-          name="Cached endpoint response (bulk fetch)")
-@limiter.limit("5/second")
-async def player_bulk(player_tags: List[str], request: Request, resonse: Response):
-    cache_data = await redis.mget(keys=[fix_tag(tag) for tag in player_tags])
-    modified_result = {}
-    for data in cache_data:
-        if data is None:
-            continue
-        data = ujson.loads(data)
-        modified_result[data.get("tag")] = data
-    return modified_result
+          name="Cached endpoint response (bulk fetch)",
+          include_in_schema=False)
+@limiter.limit("15/second")
+async def player_bulk(player_tags: List[str], api_keys: List[str], request: Request, response: Response):
+    async def get_player_responses(keys: deque, tags: list[str]):
+        tasks = []
+        connector = aiohttp.TCPConnector(limit=2000, ttl_dns_cache=300)
+        timeout = aiohttp.ClientTimeout(total=1800)
+        cached_responses = await redis.mget(keys=player_tags)
+        tag_map = {tag: r for tag, r in zip(tags, cached_responses)}
+
+        missing_tags = [t for t, r in tag_map.items() if r is None]
+        results = []
+        if missing_tags:
+            async with aiohttp.ClientSession(connector=connector, timeout=timeout) as session:
+                for tag in missing_tags:
+                    keys.rotate(1)
+                    async def fetch(url, session: aiohttp.ClientSession, headers: dict, tag: str):
+                        async with session.get(url, headers=headers) as new_response:
+                            if new_response.status != 200:
+                                return (tag, None)
+                            new_response = await new_response.read()
+                            return (tag, new_response)
+                    tasks.append(fetch(url=f'https://api.clashofclans.com/v1/players/{tag.replace("#", "%23")}', session=session, headers={"Authorization": f"Bearer {keys[0]}"}, tag=tag))
+                results = await asyncio.gather(*tasks, return_exceptions=True)
+                await session.close()
+
+        for tag, result in results:
+            tag_map[tag] = result
+        return tag_map
+
+    tag_map = await get_player_responses(keys=deque(api_keys), tags=player_tags)
+    return tag_map

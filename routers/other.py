982804d00c1a9164b20ed5fbe53ca70995a66d26
Magic <82341152+MagicTheDev@users.noreply.github.com>
2024-03-27 19:29:43 -0500

    initial

--- routers/other.py
+++ routers/other.py
@@ -9,7 +9,7 @@ from fastapi_cache.decorator import cache
 from typing import List
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
-from utils.utils import fix_tag
+from utils.utils import fix_tag, db_client
 
 limiter = Limiter(key_func=get_remote_address)
 router = APIRouter(tags=["Other"])

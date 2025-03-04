e0b07aea40ef133560464e647d4c8d7ad4454909
Destinea <valentine-gilles@orange.fr>
2025-03-04 14:08:58 +0100

    fix: coc accounts db

--- utils/utils.py
+++ utils/utils.py
@@ -107,7 +107,7 @@ class DBClient():
         self.app_users: collection_class = self.app.users
         self.app_discord_tokens: collection_class = self.app.discord_tokens
         self.app_refresh_tokens: collection_class = self.app.refresh_tokens
-        self.user_clash_accounts: collection_class = client.clashking.coc_accounts
+        self.coc_accounts: collection_class = client.clashking.coc_accounts
 
 db_client = DBClient()
 

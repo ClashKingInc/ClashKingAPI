8cf12bf7c888ef2279de4777e248861025be0ea5
Destinea <valentine-gilles@orange.fr>
2025-04-07 23:10:06 +0200

    feat: stars by TH level

--- routers/v2/war/utils.py
+++ routers/v2/war/utils.py
@@ -182,25 +182,31 @@ async def init_clan_summary_map(league_info):
             "total_destruction_inflicted": 0.0,
             "wars_played": 0,
             "town_hall_levels": {},
+            "own_th_level_list_attack": [],
+            "opponent_th_level_list_attack": [],
+            "own_th_level_list_defense": [],
+            "attacker_th_level_list_defense": [],
             "members": defaultdict(lambda: {
                 "name": None,
                 "map_position": None,
                 "avg_opponent_position": None,
                 "avg_attack_order": None,
                 "stars": 0,
-                "3_stars": 0,
-                "2_stars": 0,
-                "1_star": 0,
-                "0_star": 0,
+                "3_stars": {},
+                "2_stars": {},
+                "1_star": {},
+                "0_star": {},
+                "stars_by_th": {},
+                "defense_stars_by_th": {},
                 "total_destruction": 0.0,
                 "attack_count": 0,
                 "missed_attacks": 0,
                 "missed_defenses": 0,
                 "defense_stars_taken": 0,
-                "defense_3_stars": 0,
-                "defense_2_stars": 0,
-                "defense_1_star": 0,
-                "defense_0_star": 0,
+                "defense_3_stars": {},
+                "defense_2_stars": {},
+                "defense_1_star": {},
+                "defense_0_star": {},
                 "defense_total_destruction": 0.0,
                 "defense_count": 0
             })
@@ -231,14 +237,24 @@ async def process_war_stats(war_league_infos, clan_summary_map):
                 stats = summary["members"][mtag]
                 stats["name"] = name
 
+                # Initialize lists if not present
+                if "own_th_level_list_attack" not in stats:
+                    stats["own_th_level_list_attack"] = []
+                if "opponent_th_level_list_attack" not in stats:
+                    stats["opponent_th_level_list_attack"] = []
+                if "own_th_level_list_defense" not in stats:
+                    stats["own_th_level_list_defense"] = []
+                if "attacker_th_level_list_defense" not in stats:
+                    stats["attacker_th_level_list_defense"] = []
+
                 if "map_position_list" not in stats:
                     stats["map_position_list"] = []
                     stats["opponent_position_list"] = []
                     stats["opponent_th_level_list"] = []
                     stats["attack_order_list"] = []
-                    stats["avg_attacker_position"] = avg_pos_map[mtag]["avg_attacker_position"]
-                    stats["avg_defense_order"] = avg_pos_map[mtag]["avg_defense_order"]
-                    stats["avg_attacker_townhall_level"] = avg_pos_map[mtag]["avg_attacker_townhall_level"]
+                    stats["attacker_position_list"] = []
+                    stats["defense_order_list"] = []
+                    stats["attacker_th_level_list"] = []
 
                 if mtag in avg_pos_map:
                     stats = summary["members"][mtag]
@@ -256,44 +272,69 @@ async def process_war_stats(war_league_infos, clan_summary_map):
                         stats["opponent_th_level_list"].append(data["avg_opponent_townhall_level"])
                     if data["avg_attack_order"] is not None:
                         stats["attack_order_list"].append(data["avg_attack_order"])
+                    if data["avg_attacker_position"] is not None:
+                        stats["attacker_position_list"].append(data["avg_attacker_position"])
+                    if data["avg_defense_order"] is not None:
+                        stats["defense_order_list"].append(data["avg_defense_order"])
+                    if data["avg_attacker_townhall_level"] is not None:
+                        stats["attacker_th_level_list"].append(data["avg_attacker_townhall_level"])
 
                 attack = member.get("attacks")
                 if attack:
                     attack = attack[0] if isinstance(attack, list) else attack
                     stars = attack["stars"]
                     destruction = attack["destructionPercentage"]
+                    defender_tag = attack.get("defenderTag")
+                    defender_th = None
+                    own_th = member.get("townhallLevel")
+
+                    if defender_tag and war["opponent" if side == "clan" else "clan"]:
+                        for opp_member in war["opponent" if side == "clan" else "clan"]["members"]:
+                            if opp_member["tag"] == defender_tag:
+                                defender_th = opp_member.get("townhallLevel")
+                                break
+
+                    if defender_th is not None:
+                        stats["stars_by_th"].setdefault(stars, {}).setdefault(defender_th, 0)
+                        stats["stars_by_th"][stars][defender_th] += 1
+                        stats["opponent_th_level_list_attack"].append(defender_th)
+                        stats["own_th_level_list_attack"].append(own_th)
+
                     stats["stars"] += stars
                     stats["total_destruction"] += destruction
                     stats["attack_count"] += 1
                     summary["total_destruction_inflicted"] += destruction
                     summary["attack_count"] += 1
-                    if stars == 3:
-                        stats["3_stars"] += 1
-                    elif stars == 2:
-                        stats["2_stars"] += 1
-                    elif stars == 1:
-                        stats["1_star"] += 1
-                    else:
-                        stats["0_star"] += 1
-                elif war.get("state") == "warEnded":
-                    stats["missed_attacks"] += 1
-                    summary["missed_attacks"] += 1
+
+                else:
+                    if war.get("state") == "warEnded":
+                        stats["missed_attacks"] += 1
+                        summary["missed_attacks"] += 1
 
                 defense = member.get("bestOpponentAttack")
                 if defense:
                     stars = defense["stars"]
+                    attacker_tag = defense.get("attackerTag")
+                    attacker_th = None
+                    defender_th = member.get("townhallLevel")
+
+                    if attacker_tag and war["opponent" if side == "clan" else "clan"]:
+                        for opp_member in war["opponent" if side == "clan" else "clan"]["members"]:
+                            if opp_member["tag"] == attacker_tag:
+                                attacker_th = opp_member.get("townhallLevel")
+                                break
+
+                    if attacker_th is not None:
+                        stats["defense_stars_by_th"].setdefault(stars, {}).setdefault(attacker_th, 0)
+                        stats["defense_stars_by_th"][stars][attacker_th] += 1
+                        stats["attacker_th_level_list_defense"].append(attacker_th)
+                        stats["own_th_level_list_defense"].append(defender_th)
+
                     stats["defense_stars_taken"] += stars
                     stats["defense_total_destruction"] += defense["destructionPercentage"]
                     stats["defense_count"] += 1
                     summary["total_destruction"] += defense["destructionPercentage"]
-                    if stars == 3:
-                        stats["defense_3_stars"] += 1
-                    elif stars == 2:
-                        stats["defense_2_stars"] += 1
-                    elif stars == 1:
-                        stats["defense_1_star"] += 1
-                    else:
-                        stats["defense_0_star"] += 1
+
                 else:
                     stats["missed_defenses"] += 1
                     summary["missed_defenses"] += 1
@@ -315,6 +356,8 @@ async def compute_clan_ranking(clan_summary_map):
 
 
 def compute_member_position_stats(war, clan_key="clan", opponent_key="opponent"):
+    from collections import defaultdict
+
     enemy_map = {
         member["tag"]: member.get("mapPosition")
         for member in war[opponent_key]["members"]
@@ -342,34 +385,52 @@ def compute_member_position_stats(war, clan_key="clan", opponent_key="opponent")
         defense_orders = []
         attacker_th_levels = []
 
+        stars_by_th = defaultdict(lambda: defaultdict(int))
+        defense_stars_by_th = defaultdict(lambda: defaultdict(int))
+
         for attack in attacks:
             defender_tag = attack.get("defenderTag")
             if defender_tag in enemy_map:
                 opponent_positions.append(enemy_map[defender_tag])
             if defender_tag in enemy_townhall_map:
-                opponent_th_levels.append(enemy_townhall_map[defender_tag])
+                th_level = enemy_townhall_map[defender_tag]
+                opponent_th_levels.append(th_level)
+                stars = attack.get("stars")
+                if stars is not None:
+                    stars_by_th[stars][th_level] += 1
             if "order" in attack:
                 attack_orders.append(attack["order"])
 
-        # Defensive stats
         if defense:
             attacker_tag = defense.get("attackerTag")
             if attacker_tag in enemy_map:
                 defense_positions.append(enemy_map[attacker_tag])
             if attacker_tag in enemy_townhall_map:
-                attacker_th_levels.append(enemy_townhall_map[attacker_tag])
+                th_level = enemy_townhall_map[attacker_tag]
+                attacker_th_levels.append(th_level)
+                stars = defense.get("stars")
+                if stars is not None:
+                    defense_stars_by_th[stars][th_level] += 1
             if "order" in defense:
                 defense_orders.append(defense["order"])
 
         result[tag] = {
             "map_position": position,
             "avg_townhall_level": townhall,
-            "avg_opponent_position": round(sum(opponent_positions) / len(opponent_positions), 1) if opponent_positions else None,
-            "avg_opponent_townhall_level": round(sum(opponent_th_levels) / len(opponent_th_levels), 1) if opponent_th_levels else None,
+            "avg_opponent_position": round(sum(opponent_positions) / len(opponent_positions),
+                                           1) if opponent_positions else None,
+            "avg_opponent_townhall_level": round(sum(opponent_th_levels) / len(opponent_th_levels),
+                                                 1) if opponent_th_levels else None,
             "avg_attack_order": round(sum(attack_orders) / len(attack_orders), 1) if attack_orders else None,
-            "avg_attacker_position": round(sum(defense_positions) / len(defense_positions), 1) if defense_positions else None,
+            "avg_attacker_position": round(sum(defense_positions) / len(defense_positions),
+                                           1) if defense_positions else None,
             "avg_defense_order": round(sum(defense_orders) / len(defense_orders), 1) if defense_orders else None,
-            "avg_attacker_townhall_level": round(sum(attacker_th_levels) / len(attacker_th_levels), 1) if attacker_th_levels else None,
+            "avg_attacker_townhall_level": round(sum(attacker_th_levels) / len(attacker_th_levels),
+                                                 1) if attacker_th_levels else None,
+            "opponent_th_levels": opponent_th_levels,
+            "attacker_th_levels": attacker_th_levels,
+            "stars_by_th": dict(stars_by_th),
+            "defense_stars_by_th": dict(defense_stars_by_th),
         }
 
     return result
@@ -408,31 +469,53 @@ async def enrich_league_info(league_info, war_league_infos):
 
             if mtag in summary["members"]:
                 stats = summary["members"][mtag]
+
                 member.update({
                     "avgMapPosition": avg(stats.get("map_position_list", [])),
                     "avgOpponentPosition": avg(stats.get("opponent_position_list", [])),
                     "avgAttackOrder": avg(stats.get("attack_order_list", [])),
                     "avgTownHallLevel": stats.get("avg_townhall_level"),
                     "avgOpponentTownHallLevel": avg(stats.get("opponent_th_level_list", [])),
-                    "avgAttackerPosition": stats["avg_attacker_position"],
-                    "avgDefenseOrder": stats["avg_defense_order"],
-                    "avgAttackerTownHallLevel": stats["avg_attacker_townhall_level"],
+                    "avgAttackerPosition": avg(stats.get("attacker_position_list", [])),
+                    "avgDefenseOrder": avg(stats.get("defense_order_list", [])),
+                    "avgAttackerTownHallLevel": avg(stats.get("attacker_th_level_list", [])),
+                    "attackLowerTHLevel": sum(
+                        1 for own_th, enemy_th in
+                        zip(stats.get("own_th_level_list_attack", []), stats.get("opponent_th_level_list_attack", []))
+                        if enemy_th < own_th
+                    ),
+                    "attackUpperTHLevel": sum(
+                        1 for own_th, enemy_th in
+                        zip(stats.get("own_th_level_list_attack", []), stats.get("opponent_th_level_list", []))
+                        if enemy_th > own_th
+                    ),
+                    "defenseLowerTHLevel": sum(
+                        1 for own_th, enemy_th in
+                        zip(stats.get("own_th_level_list_defense", []), stats.get("attacker_th_level_list_defense", []))
+                        if enemy_th < own_th
+                    ),
+                    "defenseUpperTHLevel": sum(
+                        1 for own_th, enemy_th in
+                        zip(stats.get("own_th_level_list_defense", []), stats.get("attacker_th_level_list", []))
+                        if enemy_th > own_th
+                    ),
+
                     "attacks": {
                         "stars": stats["stars"],
-                        "3_stars": stats["3_stars"],
-                        "2_stars": stats["2_stars"],
-                        "1_star": stats["1_star"],
-                        "0_star": stats["0_star"],
+                        "3_stars": dict(stats.get("stars_by_th", {}).get(3, {})),
+                        "2_stars": dict(stats.get("stars_by_th", {}).get(2, {})),
+                        "1_star": dict(stats.get("stars_by_th", {}).get(1, {})),
+                        "0_star": dict(stats.get("stars_by_th", {}).get(0, {})),
                         "total_destruction": round(stats["total_destruction"], 2),
                         "attack_count": stats["attack_count"],
                         "missed_attacks": stats["missed_attacks"]
                     },
                     "defense": {
                         "stars": stats["defense_stars_taken"],
-                        "3_stars": stats["defense_3_stars"],
-                        "2_stars": stats["defense_2_stars"],
-                        "1_star": stats["defense_1_star"],
-                        "0_star": stats["defense_0_star"],
+                        "3_stars": dict(stats.get("defense_stars_by_th", {}).get(3, {})),
+                        "2_stars": dict(stats.get("defense_stars_by_th", {}).get(2, {})),
+                        "1_star": dict(stats.get("defense_stars_by_th", {}).get(1, {})),
+                        "0_star": dict(stats.get("defense_stars_by_th", {}).get(0, {})),
                         "total_destruction": round(stats["defense_total_destruction"], 2),
                         "defense_count": stats["defense_count"],
                         "missed_defenses": stats["missed_defenses"]
